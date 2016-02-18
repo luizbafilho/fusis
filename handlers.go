e9a1322f05c127bcc00af6dfa242b3c9876892c2
Luiz Filho <luizbafilho@gmail.com>
2016-02-17 23:45:22 -0200

    adding ipvs and netlink packages to the project

--- handlers.go
+++ handlers.go
@@ -4,12 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
-	"syscall"
 
-	"github.com/google/seesaw/ipvs"
 	"github.com/gorilla/mux"
+	"github.com/luizbafilho/janus/ipvs"
 )
 
 //Index Handles index
@@ -30,24 +28,18 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 //ServiceCreate ...
 func ServiceCreate(w http.ResponseWriter, r *http.Request) {
-	var newService Service
+	var newService ipvs.Service
 	err := json.NewDecoder(r.Body).Decode(&newService)
 	if err != nil {
 		http.Error(w, err.Error(), 400)
 		return
 	}
 
-	ipvsService := ipvs.Service{
-		Address:   net.ParseIP(newService.Host),
-		Protocol:  syscall.IPPROTO_TCP,
-		Port:      newService.Port,
-		Scheduler: newService.Scheduler,
-	}
+	fmt.Println(newService)
 
-	if err := ipvs.AddService(ipvsService); err != nil {
+	if err := ipvs.AddService(newService); err != nil {
 		log.Fatalf("ipvs.AddService() failed: %v\n", err)
 	}
-
 	w.WriteHeader(http.StatusCreated)
 }
 
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/google/seesaw/ipvs"
+	"github.com/luizbafilho/janus/ipvs"
 )
 
 func main() {

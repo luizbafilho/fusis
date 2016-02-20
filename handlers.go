4e83a5ad26982b129779b74f69009a9f8687ade6
Luiz Filho <luizbafilho@gmail.com>
2016-02-20 17:27:43 -0200

    removing unused code

--- handlers.go
+++ handlers.go
@@ -205,10 +205,3 @@ func DestinationDelete(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-
-//TodoShow expoer
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoID := vars["todoId"]
-	fmt.Fprintln(w, "Todo Fuck:", todoID)
-}

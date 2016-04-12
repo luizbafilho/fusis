890ce40f87e664f6d92397f9ea3f3bbad9578b8e
Luiz Filho <luizbafilho@gmail.com>
2016-04-12 03:34:16 +0000

    [net] adding default gateway functions

--- net/net.go
+++ net/net.go
@@ -1,25 +1,13 @@
 package net
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/vishvananda/netlink"
 )
-
-func AddDefaultGateway(ip string) error {
-	//TODO: Delete previous default gw
-	err := netlink.RouteAdd(&netlink.Route{
-		Scope: netlink.SCOPE_UNIVERSE,
-		Gw:    net.ParseIP(ip),
-	})
-	if err != nil {
-		log.Errorf("Adding Default Gateway: %s", ip)
-		return err
-	}
-	return nil
-}
 
 func AddIp(ip string, iface string) error {
 	link, err := netlink.LinkByName(iface)
@@ -52,4 +40,45 @@ func GetIpByInterface(iface string) (string, error) {
 
 func SetIpForwarding() error {
 	return ioutil.WriteFile("/proc/sys/net/ipv4/ip_forward", []byte("1"), 0644)
+}
+
+func AddDefaultGateway(ip string) error {
+	err := netlink.RouteAdd(&netlink.Route{
+		Scope: netlink.SCOPE_UNIVERSE,
+		Gw:    net.ParseIP(ip),
+	})
+	if err != nil {
+		log.Errorf("Adding Default Gateway: %s", ip)
+		return err
+	}
+	return nil
+}
+
+func GetDefaultGateway() (*netlink.Route, error) {
+	routes, err := netlink.RouteList(nil, netlink.FAMILY_V4)
+	if err != nil {
+		return nil, err
+	}
+
+	var route netlink.Route
+	for _, v := range routes {
+		if v.Gw != nil {
+			route = v
+		}
+	}
+
+	if route.Gw == nil {
+		return nil, fmt.Errorf("default gateway not found")
+	}
+
+	return &route, nil
+}
+
+func DeleteDefaultGateway(route *netlink.Route) error {
+	err := netlink.RouteDel(route)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }

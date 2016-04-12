81503e1d88e72755fe2abc655864e29b9cf227ce
Luiz Filho <luizbafilho@gmail.com>
2016-04-12 02:51:09 +0000

    [config] Refactoring config struct

--- fusis/config.go
+++ fusis/config.go
@@ -2,26 +2,26 @@ package fusis
 
 import "github.com/luizbafilho/fusis/net"
 
-type BalancerConfig struct {
+type Config struct {
 	Interface string
+}
+
+type BalancerConfig struct {
+	Config
 }
 
 type AgentConfig struct {
-	Balancer  string
-	Name      string
-	Host      string
-	Port      uint16
-	Weight    int32
-	Mode      string
-	Service   string
-	Interface string
+	Config
+
+	Balancer string
+	Name     string
+	Host     string
+	Port     uint16
+	Weight   int32
+	Mode     string
+	Service  string
 }
 
-// It returns the first IP for a given network interface
-func (c *AgentConfig) GetIpByInterface() (string, error) {
-	return net.GetIpByInterface(c.Interface)
-}
-
-func (c *BalancerConfig) GetIpByInterface() (string, error) {
+func (c *Config) GetIpByInterface() (string, error) {
 	return net.GetIpByInterface(c.Interface)
 }

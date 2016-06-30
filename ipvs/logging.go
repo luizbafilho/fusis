a2b91a4c46a50787f5ef42e3830f38fb79af7912
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-06-30 19:55:30 -0300

    Created logging functions. RouterLog is the default

---
+++ ipvs/logging.go
@@ -0,0 +1,48 @@
+package ipvs
+
+import(
+    "time"
+    "strings"
+
+	"github.com/Sirupsen/logrus"
+    "github.com/bshuster-repo/logrus-logstash-hook"
+
+	"github.com/luizbafilho/fusis/api/types"
+)
+
+
+func RouterLog (statsLog *logrus.Logger, tick time.Time, s types.Service) {
+
+    hosts := []string{}
+    for _, dst := range s.Destinations {
+        hosts = append(hosts, dst.Host)
+    }
+
+    statsLog.WithFields(logrus.Fields{
+       "time": tick,
+       "service": s.Name,
+       "Protocol": s.Protocol,
+       "Port": s.Port,
+       "hosts": strings.Join(hosts, ","),
+       "client": "fusis",
+    }).Info("Fusis router stats")
+}
+
+
+func LogStash () {
+
+    // TODO: Generalise this options to come from fusis.json.
+    // At engine.go verify the log type and set the logger
+
+    logger := logrus.New()
+    PROTOCOL := "udp"
+    HOST := "logstash.video.dev.globoi.com"
+    PORT := "8515"
+    url := []string{HOST, ":", PORT}
+    hook, err := logrus_logstash.NewHook(PROTOCOL, strings.Join(url, ""), "Fusis")
+    if err != nil {
+        logger.Fatal(err)
+    }
+
+    logger.Hooks.Add(hook)
+}

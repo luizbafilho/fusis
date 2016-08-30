baf5c36b20e649d45ce7f2e043b957025bed0bc9
Luiz Filho <luizbafilho@gmail.com>
2016-08-30 02:19:40 +0000

    Acception multiples range to be used by IPAM

--- ipam/ipam.go
+++ ipam/ipam.go
@@ -2,6 +2,7 @@ package ipam
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/luizbafilho/fusis/api/types"
 	"github.com/luizbafilho/fusis/config"
@@ -26,7 +27,7 @@ var (
 
 //Init initilizes ipam module
 func New(state *state.State, config *config.BalancerConfig) (Allocator, error) {
-	rangeCursor, err := ipaddr.Parse(config.Ipam.Ranges[0])
+	rangeCursor, err := ipaddr.Parse(strings.Join(config.Ipam.Ranges, ","))
 	if err != nil {
 		return nil, err
 	}
@@ -37,6 +38,12 @@ func New(state *state.State, config *config.BalancerConfig) (Allocator, error) {
 //Allocate allocates a new avaliable ip
 func (i *Ipam) AllocateVIP(s *types.Service) error {
 	for pos := i.rangeCursor.Next(); pos != nil; pos = i.rangeCursor.Next() {
+		//TODO: make it compatible if IPv6
+		// Verifies if it is a base IP address. example: 10.0.100.0; 192.168.1.0
+		if pos.IP.To4()[3] == 0 {
+			pos = i.rangeCursor.Next()
+		}
+
 		assigned := i.ipIsAssigned(pos.IP.String(), i.state)
 
 		if !assigned {
--- ipam/ipam_test.go
+++ ipam/ipam_test.go
@@ -1,6 +1,7 @@
 package ipam_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/luizbafilho/fusis/api/types"
@@ -24,7 +25,7 @@ func (s *IpamSuite) SetUpSuite(c *C) {
 	var err error
 	config := &config.BalancerConfig{
 		Ipam: config.Ipam{
-			Ranges: []string{"192.168.0.0/28"},
+			Ranges: []string{"192.168.0.0/30", "10.0.1.0/30"},
 		},
 	}
 
@@ -41,21 +42,40 @@ func (s *IpamSuite) TearDownSuite(c *C) {
 func (s *IpamSuite) TestIpAllocation(c *C) {
 	service := &types.Service{
 		Name: "test",
-		Host: "192.168.0.2",
+		Host: "10.0.1.2",
 	}
 	s.state.AddService(service)
 
 	err := s.ipam.AllocateVIP(service)
 	c.Assert(err, IsNil)
-	c.Assert(service.Host, Equals, "192.168.0.1")
+	c.Assert(service.Host, Equals, "10.0.1.1")
 
 	service = &types.Service{
 		Name: "test2",
-		Host: "192.168.0.1",
+		Host: "10.0.1.1",
 	}
 	s.state.AddService(service)
 
 	err = s.ipam.AllocateVIP(service)
 	c.Assert(err, IsNil)
-	c.Assert(service.Host, Equals, "192.168.0.3")
+	c.Assert(service.Host, Equals, "10.0.1.3")
+}
+
+func (s *IpamSuite) TestIpAllocationMultiplesRanges(c *C) {
+	ips := []string{"10.0.1.1", "10.0.1.2", "10.0.1.3"}
+
+	for i, v := range ips {
+		s.state.AddService(&types.Service{
+			Name: fmt.Sprintf("test%s", i),
+			Host: v,
+		})
+	}
+
+	service := &types.Service{
+		Name: "test10",
+	}
+	err := s.ipam.AllocateVIP(service)
+	c.Assert(err, IsNil)
+	c.Assert(service.Host, Equals, "192.168.0.1")
+
 }

02ce4bc924392d738fdad1f25061db4c255803e6
Luiz Filho <luizbafilho@gmail.com>
2016-09-17 15:55:12 +0000

    Adding systcl methods

---
+++ net/sysctl_test.go
@@ -0,0 +1,29 @@
+package net
+
+import "testing"
+
+func TestGetSysctl(t *testing.T) {
+	got, err := GetSysctl("net.ipv4.tcp_sack")
+	ex := "1"
+	if err != nil {
+		t.Fatalf("Could not read key")
+	}
+	if got != ex {
+		t.Errorf("Expected: %s, got %s", ex, got)
+	}
+}
+
+func TestSetSysctl(t *testing.T) {
+	ex := "0"
+	err := SetSysctl("net.ipv4.ip_forward", ex)
+	if err != nil {
+		t.Fatalf("err")
+	}
+	got, err := GetSysctl("net.ipv4.ip_forward")
+	if err != nil {
+		t.Fatalf("Could not read key")
+	}
+	if got != ex {
+		t.Errorf("Expected: %s, got %s", ex, got)
+	}
+}

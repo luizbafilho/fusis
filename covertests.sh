43e06f83b5100757ecffa026604a6347dbf9719d
Luiz Filho <luizbafilho@gmail.com>
2017-11-02 22:13:39 -0300

    Making travis happy

--- covertests.sh
+++ covertests.sh
@@ -3,10 +3,10 @@
 echo "" > coverage.txt
 code=0
 for pkg in $(go list ./... | grep -v /vendor); do
-    sudo -E go test -race -coverprofile=profile.out -covermode=atomic $pkg || code=$?
+    go test -race -coverprofile=profile.out -covermode=atomic $pkg || code=$?
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
-        sudo rm -f profile.out
+        rm -f profile.out
     fi
 done
 (exit $code)
